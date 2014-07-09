ea30d3359185c9c2d73a04a798eca18ddc3ad6d8
Sravan Reddy <sravan.reddy@oracle.com>
2014-07-09 04:42:38 -0700

    corrected the test parameters.

--- savedsearch-web/src/test/java/oracle/sysman/emSDK/emaas/platform/savedsearch/test/common/CommonTest.java
+++ savedsearch-web/src/test/java/oracle/sysman/emSDK/emaas/platform/savedsearch/test/common/CommonTest.java
@@ -1,62 +1,65 @@
 package oracle.sysman.emSDK.emaas.platform.savedsearch.test.common;
 
-import static com.jayway.restassured.RestAssured.config;
-import static com.jayway.restassured.config.LogConfig.logConfig;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.config.LogConfig;
 
-public class CommonTest {
+public class CommonTest
+{
 
-	private String HOSTNAME;
-	private String portno;
-	private String serveruri;
+	private final String HOSTNAME;
+	private final String portno;
+	private final String serveruri;
 
 	/**
-	 * Sets up RESTAssured defaults before executing test cases Enables logging
-	 * Reading the inputs from the testenv.properties file
+	 * Sets up RESTAssured defaults before executing test cases Enables logging Reading the inputs from the testenv.properties
+	 * file
 	 */
 
-	public CommonTest() {
+	public CommonTest()
+	{
 		Properties prop = new Properties();
 		InputStream input = null;
 		try {
 			input = new FileInputStream("testenv.properties");
 			prop.load(input);
 			System.out.println("---------------------------------------------------------------------");
-			System.out.println("The property values - Hostname: "
-					+ prop.getProperty("Hostname")+ " and Port: " +prop.getProperty("Port"));
+			System.out.println("The property values - Hostname: " + prop.getProperty("hostname") + " and Port: "
+					+ prop.getProperty("port"));
 			System.out.println("---------------------------------------------------------------------");
 			System.out.println("											");
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 
 			e.printStackTrace();
 		}
 
-		HOSTNAME = prop.getProperty("Hostname");
-		portno = prop.getProperty("Port");
+		HOSTNAME = prop.getProperty("hostname");
+		portno = prop.getProperty("port");
 		serveruri = "http://" + HOSTNAME + ":" + portno;
 		RestAssured.useRelaxedHTTPSValidation();
 		RestAssured.baseURI = serveruri;
 		RestAssured.basePath = "/savedsearch/v1";
-		RestAssured.config = config().logConfig(
-				logConfig().enablePrettyPrinting(false));
+		RestAssured.config = RestAssured.config().logConfig(LogConfig.logConfig().enablePrettyPrinting(false));
 
 	}
 
-	public String getHOSTNAME() {
+	public String getHOSTNAME()
+	{
 		return HOSTNAME;
 	}
 
-	public String getPortno() {
+	public String getPortno()
+	{
 		return portno;
 	}
 
-	public String getServeruri() {
+	public String getServeruri()
+	{
 		return serveruri;
 	}
 
