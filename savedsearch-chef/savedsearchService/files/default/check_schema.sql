165b804b3768c9c23e6f6508292d5af5a742584e
Sachin Dhamdhere <sachin.dhamdhere@oracle.com>
2014-07-24 19:48:39 -0700

    EMCLAS-1385: DEPLOYMENT RECIPE FOR SAVED SEARCH NEEDS TO BE NON DATA DESTRUCTIVE RE-ENTRANT
    SQL scripts are already non-destructive
    This change checks for schema existence & skips steps if schema exists (tests by running a query against a known table)
    
    The seed-data upgrades will be dealt with upgrade process.
    
    Enable editions on this schema

---
+++ savedsearch-chef/savedsearchService/files/default/check_schema.sql
@@ -0,0 +1,13 @@
+--
+-- Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+--
+-- Checks if the saved search schema is present on this db
+--
+
+-- Exit with error if expected table is not present
+WHENEVER SQLERROR EXIT 1;
+
+-- Check the presence of one of the last tables created by the schema creation scripts
+SELECT 'SAVED_SEARCH_SCHEMA_OK=' || TO_CHAR(0*COUNT(*)) FROM EMS_ANALYTICS_CATEGORY;
+
+EXIT 0;
