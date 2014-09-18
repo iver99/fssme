19ff12c373b90ca6a6c97ea5b25ef2ceff52ae15
Sachin Dhamdhere <sachin.dhamdhere@oracle.com>
2014-09-18 07:45:31 -0700

    fixed comments

--- savedsearch-schema/src/main/sql/upgrade/upgrade_055_06_dml.sql
+++ savedsearch-schema/src/main/sql/upgrade/upgrade_055_06_dml.sql
@@ -7,13 +7,13 @@ Rem    NAME
 Rem      upgrade_05_055.sql
 Rem
 Rem    DESCRIPTION
-Rem      Upgrade file for upgrading from v0.5 to v0.5.5 - do NOT run this file multiple times
+Rem      Upgrade file for upgrading from v0.5.5 to v0.6 
 Rem
 Rem    NOTES
 Rem      This file will get called via generic upgrade.sql - upgrade.sql gets called as part of running SSF deployment recipe
 Rem
 Rem    MODIFIED   (MM/DD/YY)
-Rem    sdhamdhe		09/10/14 - New OOB searches for TA
+Rem    sdhamdhe		09/18/14 - placeholder  
 Rem
 
 -- re-entrant-ness
