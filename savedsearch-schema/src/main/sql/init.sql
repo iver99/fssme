0b621abe21218933a4c05ce96e9df29142b30c75
Vinay Joshi <vinay.joshi@oracle.com>
2015-01-27 00:09:17 -0800

    provided default values

--- savedsearch-schema/src/main/sql/init.sql
+++ savedsearch-schema/src/main/sql/init.sql
@@ -2,10 +2,6 @@
 @./emaas_savesearch_seq.sql
 @./emaas_savesearch_index.sql
 @./emaas_savesearch_triggers.sql
-@./emaas_savesearch_seed_data.sql
 
-@./emaas_savesearch_seed_data_ta.sql
-@./emaas_savesearch_seed_data_la.sql
-@./emaas_savesearch_seed_data_apm.sql
 
 
