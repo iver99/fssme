fd17863c0a4bab1b1cdbbbbf22ab1aa40baebd96
Bill Keicher <william.keicher@oracle.com>
2015-02-20 09:30:59 -0500

    ta oob saved widgets

--- savedsearch-schema/src/main/sql/upgrade/upgrade_058_059_ta_dml.sql
+++ savedsearch-schema/src/main/sql/upgrade/upgrade_058_059_ta_dml.sql
@@ -1,23 +1,22 @@
+Rem  drv: <migrate type="data_upgrade" version="13.1.0.0" />
 Rem
+Rem upgrade_058_059_ta_dml.sql
 Rem
 Rem Copyright (c) 2013, 2014, Oracle and/or its affiliates. 
 Rem All rights reserved.
 Rem
 Rem    NAME
-Rem      upgrade_056_057_ta_dml.sql
+Rem      upgrade_058_059_ta_dml.sql 
 Rem
 Rem    DESCRIPTION
-Rem      Upgrade file for upgrading from v0.5.6 to v0.5.7 
+Rem      Target Analytics upgrade
 Rem
 Rem    NOTES
-Rem      This file will get called via generic upgrade.sql - upgrade.sql gets called as part of running SSF deployment recipe
+Rem      None
 Rem
 Rem    MODIFIED   (MM/DD/YY)
-Rem    WKEICHER		09/30/14 - Upgrade TA OOB Searches  
+Rem    WKEICHER    02/18/15 - Update TA OOB Searches
 Rem
-
-
-
 
 delete from EMS_ANALYTICS_LAST_ACCESS where OBJECT_ID>=3000 and OBJECT_ID<=3999 and OBJECT_TYPE=2 ;
 delete from EMS_ANALYTICS_SEARCH_PARAMS where SEARCH_ID >= 3000 and SEARCH_ID <= 3999 ;
