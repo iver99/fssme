6250b1c6553ecc0b1046f6b84091f12c6ba637eb
Vinay Joshi <vinay.joshi@oracle.com>
2015-05-15 12:05:27 -0700

    emcpssf-133 fixed

--- savedsearch-tools/src/integTest/java/oracle/sysman/emSDK/emaas/platform/updatesearch/test/common/CommonTest.java
+++ savedsearch-tools/src/integTest/java/oracle/sysman/emSDK/emaas/platform/updatesearch/test/common/CommonTest.java
@@ -19,9 +19,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import oracle.sysman.qatool.uifwk.utils.Utils;
+
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.config.LogConfig;
-import oracle.sysman.qatool.uifwk.utils.Utils;
+
 public class CommonTest
 {
 
@@ -29,6 +31,8 @@ public class CommonTest
 	private final String portno;
 	private final String serveruri;
 	private final String authToken;
+	private final String tenantid;
+	private final String remoteuser;
 
 	/**
 	 * Sets up RESTAssured defaults before executing test cases Enables logging Reading the inputs from the testenv.properties
@@ -63,12 +67,14 @@ public class CommonTest
 			}
 		}
 
-	//	HOSTNAME = prop.getProperty("hostname");
-	HOSTNAME=Utils.getProperty("EMCS_NODE1_HOSTNAME");
-	authToken=Utils.getProperty("SAAS_AUTH_TOKEN");
-	//	portno = prop.getProperty("port");
+		/*OSTNAME = prop.getProperty("hostname");
+		portno = prop.getProperty("port");
+		authToken = prop.getProperty("authToken");*/
+		HOSTNAME = Utils.getProperty("EMCS_NODE1_HOSTNAME");
+		authToken = Utils.getProperty("SAAS_AUTH_TOKEN");
 		portno = "7001";
-	//authToken = prop.getProperty("authToken");
+		tenantid = Utils.getProperty("TENANT_ID");
+		remoteuser = Utils.getProperty("SSO_USERNAME");
 		serveruri = "http://" + HOSTNAME + ":" + portno;
 		RestAssured.useRelaxedHTTPSValidation();
 		RestAssured.baseURI = serveruri;
@@ -92,9 +98,19 @@ public class CommonTest
 		return portno;
 	}
 
+	public String getRemoteUser()
+	{
+		return remoteuser;
+	}
+
 	public String getServeruri()
 	{
 		return serveruri;
+	}
+
+	public String getTenant()
+	{
+		return tenantid;
 	}
 
 }
