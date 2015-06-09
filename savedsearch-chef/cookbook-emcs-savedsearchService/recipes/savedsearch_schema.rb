10c3936ed626cbdaa769b33fa9630ddbe088fb93
Vinay Joshi <vinay.joshi@oracle.com>
2015-06-09 03:19:30 -0700

    Cut-over to LCMRepManager for Schema Lifecycle Management

--- savedsearch-chef/cookbook-emcs-savedsearchService/recipes/savedsearch_schema.rb
+++ savedsearch-chef/cookbook-emcs-savedsearchService/recipes/savedsearch_schema.rb
@@ -34,6 +34,17 @@ cookbook_file "check_schema.sql" do
   action :create_if_missing
 end
 
+#
+product_schema_name = "SYSEMS_T_#{node["SAAS_schema_id"]}" 
+                  loadResourceCredential "Load Product Schema Credential" do
+                     credentialType "SchemaCredentials"
+                     credentialName "#{product_schema_name}"
+                  end
+
+product_schema_password = node["SchemaCredentials"]["#{product_schema_name}"]["userCredential"]
+#
+
+
 # Check existence of schema
 #
 bash "check_schema" do
@@ -48,7 +59,7 @@ bash "check_schema" do
     if [ -e #{schema_exists_file} ] ; then
       rm -f #{schema_exists_file}
     fi
-    check_schema_result=`#{node["dbhome"]}/bin/sqlplus -s #{node["SAAS_schema_user"]}/#{node["SAAS_schema_password"]}@#{node["db_host"]}:#{node["db_port"]}/#{node["db_service"]} @#{check_schema_file} | grep "SAVED_SEARCH_SCHEMA_OK=0"`
+    check_schema_result=`#{node["dbhome"]}/bin/sqlplus -s #{product_schema_user}/#{product_schema_password}@'#{node["database_ConnectString"]}' @#{check_schema_file} | grep "SAVED_SEARCH_SCHEMA_OK=0"`
     echo "check schema result = '$check_schema_result'" >> #{node["log_dir"]}/savedSearchDatasource.log
     if [ -n "$check_schema_result" ] ; then
       echo $check_schema_result > #{schema_exists_file}
@@ -75,26 +86,17 @@ echo "Version: #{node["SAAS_version"]}" >> #{node["log_dir"]}/savedSearchDatasou
 echo "SQL Dir: #{node["sql_dir"]}" >> #{node["log_dir"]}/savedSearchDatasource.log
 cd #{node["apps_dir"]}/#{node["SAAS_servicename"]}/#{node["SAAS_version"]}/#{node["sql_dir"]}
 
-echo "db_servicename = #{node["db_service"]}, SAAS_schema_user = #{node["SAAS_schema_user"]}, SYS_password = #{node["db_syspassword"]} db_port=#{node["db_port"]} db_host=#{node["db_host"]} home=#{node["dbhome"]}" >> #{node["log_dir"]}/savedSearchDatasource.log
+
+
+echo "db_servicename = #{node["db_service"]}, SAAS_schema_user = #{product_schema_user}, SYS_password = #{node["db_syspassword"]} db_port=#{node["db_port"]} db_host=#{node["db_host"]} home=#{node["dbhome"]}" >> #{node["log_dir"]}/savedSearchDatasource.log
 export LD_LIBRARY_PATH=#{node["dbhome"]}/lib
-
-echo "#{node["dbhome"]}/bin/sqlplus #{node["sys_user"]}/#{node["db_syspassword"]}@#{node["db_host"]}:#{node["db_port"]}/#{node["db_service"]} as sysdba" >> #{node["log_dir"]}/savedSearchDatasource.log
-#{node["dbhome"]}/bin/sqlplus #{node["sys_user"]}/#{node["db_syspassword"]}@#{node["db_host"]}:#{node["db_port"]}/#{node["db_service"]} as sysdba << disp > #{node["log_dir"]}/savedSearchsql.txt 2>&1 >> #{node["log_dir"]}/savedSearchDatasource.log
-CREATE USER #{node["SAAS_schema_user"]} identified by #{node["SAAS_schema_password"]} ENABLE EDITIONS;
-ALTER USER #{node["SAAS_schema_user"]} default tablespace users temporary tablespace temp;
-GRANT CONNECT,RESOURCE TO #{node["SAAS_schema_user"]};
-GRANT CREATE SESSION TO #{node["SAAS_schema_user"]};
-GRANT UNLIMITED TABLESPACE TO #{node["SAAS_schema_user"]};
-disp
-echo "done with creating user" >> #{node["log_dir"]}/savedSearchDatasource.log
-
 echo "running the script now" >> #{node["log_dir"]}/savedSearchDatasource.log
-echo "#{node["dbhome"]}/bin/sqlplus #{node["SAAS_schema_user"]}/#{node["SAAS_schema_password"]}@#{node["db_host"]}:#{node["db_port"]}/#{node["db_service"]}" >> #{node["log_dir"]}/savedSearchDatasource.log
+echo "#{node["dbhome"]}/bin/sqlplus #{product_schema_user}/#{product_schema_name}@'#{node["database_ConnectString"]}'" >> #{node["log_dir"]}/savedSearchDatasource.log
 echo "CWD:" >> #{node["log_dir"]}/savedSearchDatasource.log
 pwd >> #{node["log_dir"]}/savedSearchDatasource.log
 for file in init.sql
 do
-#{node["dbhome"]}/bin/sqlplus #{node["SAAS_schema_user"]}/#{node["SAAS_schema_password"]}@#{node["db_host"]}:#{node["db_port"]}/#{node["db_service"]} << eof_sql > #{node["log_dir"]}/savedSearchsql.txt 2>&1 >> #{node["log_dir"]}/savedSearchDatasource.log
+#{node["dbhome"]}/bin/sqlplus #{product_schema_user}/#{product_schema_password}@'#{node["database_ConnectString"]}' << eof_sql > #{node["log_dir"]}/savedSearchsql.txt 2>&1 >> #{node["log_dir"]}/savedSearchDatasource.log
 @$file
 eof_sql
 done
@@ -117,12 +119,12 @@ else
 	cd #{node["apps_dir"]}/#{node["SAAS_servicename"]}/#{node["SAAS_version"]}/#{node["sql_dir"]}/upgrade
 	
 	echo "Running upgrade script now" >> #{node["log_dir"]}/savedSearchDatasource.log
-	echo "#{node["dbhome"]}/bin/sqlplus #{node["SAAS_schema_user"]}/#{node["SAAS_schema_password"]}@#{node["db_host"]}:#{node["db_port"]}/#{node["db_service"]}" >> #{node["log_dir"]}/savedSearchDatasource.log
+	echo "#{node["dbhome"]}/bin/sqlplus #{product_schema_user}/#{product_schema_password}@'#{node["database_ConnectString"]}'" >> #{node["log_dir"]}/savedSearchDatasource.log
 	echo "CWD:" >> #{node["log_dir"]}/savedSearchDatasource.log
 	pwd >> #{node["log_dir"]}/savedSearchDatasource.log
 	for file in upgrade.sql
 		do
-			#{node["dbhome"]}/bin/sqlplus #{node["SAAS_schema_user"]}/#{node["SAAS_schema_password"]}@#{node["db_host"]}:#{node["db_port"]}/#{node["db_service"]} << eof_sql > #{node["log_dir"]}/savedSearchsql.txt 2>&1 >> #{node["log_dir"]}/savedSearchDatasource.log
+			#{node["dbhome"]}/bin/sqlplus #{product_schema_user}/#{product_schema_password}@'#{node["database_ConnectString"]}' << eof_sql > #{node["log_dir"]}/savedSearchsql.txt 2>&1 >> #{node["log_dir"]}/savedSearchDatasource.log
 			@$file
 eof_sql
 			done
