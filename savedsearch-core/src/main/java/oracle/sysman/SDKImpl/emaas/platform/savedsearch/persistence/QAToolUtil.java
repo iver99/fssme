fa03b64e1064866278a4736cacdc2a119988216f
Vinay Joshi <vinay.joshi@oracle.com>
2015-06-27 22:01:37 -0700

    Extracting data using regular expression and not using jackson jar

--- savedsearch-core/src/main/java/oracle/sysman/SDKImpl/emaas/platform/savedsearch/persistence/QAToolUtil.java
+++ savedsearch-core/src/main/java/oracle/sysman/SDKImpl/emaas/platform/savedsearch/persistence/QAToolUtil.java
@@ -15,8 +15,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import oracle.sysman.qatool.uifwk.utils.Utils;
 
@@ -30,14 +33,17 @@ public class QAToolUtil
 {
 
 	private static final String AUTHORIZATION = "Authorization";
+	private static final String PATTERN = "((https?|www):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
+	private static final String LIFE_CYCLE = "LifecycleInvManager";
 
 	private static final String AUTH_STRING = "Basic d2VibG9naWM6d2VsY29tZTE=";//WEBLOGIC_USER + ":" + WEBLOGIC_USER;
 	private static final String DEPLOY_URL = "/instances?servicename=LifecycleInventoryService";
 	private static final String SSF_DEPLOY_URL = "/instances?servicename=SavedSearch";
-	private static final String DEPLY_SCHEMA = "/schemaDeployments";
+	private static final String DEPLY_SCHEMA = "/schemaDeployments?softwareName=SavedSearchService";
 	private static final String SERVICE_NAME = "SavedSearchService";
 	private static final String PASSWORD = "welcome1";
 	public static final String JDBC_URL = "jdbc:oracle:thin:@";
+	private static final String SCHEMA_USER = "schemaUser";
 
 	private static final String SERVICE_MANAGER_URL = "SERVICE_MANAGER_URL";
 
@@ -77,14 +83,7 @@ public class QAToolUtil
 		String data = QAToolUtil.getDetaildByUrl(Utils.getProperty(SERVICE_MANAGER_URL) + SSF_DEPLOY_URL);
 		//String data = QAToolUtil.getDetaildByUrl("http://slc08twq.us.oracle.com:7001//registry/servicemanager/registry/v1"
 		//	+ SSF_DEPLOY_URL);
-		try {
-			long l = 1 / 0;
-		}
-		catch (Exception e) {
-			logger.error("data:" + data + " " + Utils.getProperty(SERVICE_MANAGER_URL) + SSF_DEPLOY_URL, e);
-			System.out.println("data:" + data + " " + Utils.getProperty(SERVICE_MANAGER_URL) + SSF_DEPLOY_URL + e.toString());
-		}
-		System.out.println(data);
+
 		List<String> urlList = SchemaUtil.getSchemaUrls(data);
 		if (urlList == null | urlList.isEmpty()) {
 			return null;
@@ -100,10 +99,26 @@ public class QAToolUtil
 		return props;
 	}
 
-	/*public static void main(String h[])
+	public static void main(String h[])
 	{
-		System.out.println(QAToolUtil.getSavedSearchDeploymentDet());
-	}*/
+		System.out.println(QAToolUtil.getSchemaDeploymentDetails());
+	}
+
+	private static List<String> extractUrls(String value)
+	{
+
+		if (value == null) {
+			return null;
+		}
+		List<String> result = new ArrayList<String>();
+
+		Pattern p = Pattern.compile(PATTERN, Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(value);
+		while (m.find()) {
+			result.add(value.substring(m.start(0), m.end(0)));
+		}
+		return result;
+	}
 
 	private static String getDetaildByUrl(String url)
 	{
@@ -149,25 +164,38 @@ public class QAToolUtil
 	{
 		String username = null;
 		String data = QAToolUtil.getDetaildByUrl(Utils.getProperty(SERVICE_MANAGER_URL) + DEPLOY_URL);
-		//String data = QAToolUtil.getDetaildByUrl("http://slc08twq.us.oracle.com:7001//registry/servicemanager/registry/v1"
+		//String data = QAToolUtil.getDetaildByUrl("http://slc04lec.us.oracle.com:7001//registry/servicemanager/registry/v1"
 		//	+ DEPLOY_URL);
-		try {
-			long l = 1 / 0;
-		}
-		catch (Exception e) {
-			logger.error("data:" + data + " " + Utils.getProperty(SERVICE_MANAGER_URL) + DEPLOY_URL, e);
-			System.out.println("data:" + data + " " + Utils.getProperty(SERVICE_MANAGER_URL) + DEPLOY_URL + e.toString());
-		}
-		System.out.println(data);
-		List<String> urlList = SchemaUtil.getSchemaUrls(data);
+		List<String> urlList = QAToolUtil.extractUrls(data);
 		for (String temp : urlList) {
-			data = QAToolUtil.getDetaildByUrl(temp + DEPLY_SCHEMA);
-			username = SchemaUtil.getSchemaUserBySoftwareName(data, SERVICE_NAME);
-			if (username != null) {
-				return username;
+			if (temp.endsWith(LIFE_CYCLE)) {
+				data = QAToolUtil.getDetaildByUrl(temp + DEPLY_SCHEMA);
+				if (data != null) {
+					username = QAToolUtil.getUsername(data);
+					if (username != null) {
+						return username;
+					}
+				}
 			}
 		}
 
+		return null;
+	}
+
+	private static String getUsername(String data)
+	{
+		String[] options = data.split(",");
+		if (options.length > 0) {
+			for (String tmp : options) {
+				if (tmp.contains(SCHEMA_USER)) {
+					String name = tmp.substring(tmp.indexOf(":"));
+					if (name != null) {
+						name = name.replace("\"", "").replace(":", "");
+						return name;
+					}
+				}
+			}
+		}
 		return null;
 	}
 
