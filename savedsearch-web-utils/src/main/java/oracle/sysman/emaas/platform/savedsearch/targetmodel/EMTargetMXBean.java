6e254826c0db6bb4c22135d67c71e887bbff4a43
Vinay Joshi <vinay.joshi@oracle.com>
2015-07-20 02:46:52 -0700

    JMX MBean registration for  monitoring SSF as  target in EMCC.

---
+++ savedsearch-web-utils/src/main/java/oracle/sysman/emaas/platform/savedsearch/targetmodel/EMTargetMXBean.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2015 Oracle
+ * All rights reserved.
+ *
+ * $$File: $$
+ * $$DateTime: $$
+ * $$Author: $$
+ * $$Revision: $$
+ */
+
+package oracle.sysman.emaas.platform.savedsearch.targetmodel;
+
+/**
+ * @author vinjoshi
+ *
+ */
+
+/**
+ * MXBean interface for EM Target Discovery.
+ */
+public interface EMTargetMXBean
+{
+	/**
+	 * Gets the EM target type.
+	 * 
+	 * @exception Exception
+	 *                if the operation fails
+	 */
+	public String getEMTargetType() throws Exception;
+
+	/**
+	 * Gets the EM target name.
+	 * 
+	 * @exception Exception
+	 *                if the operation fails
+	 */
+	public String getName() throws Exception;
+
+}
