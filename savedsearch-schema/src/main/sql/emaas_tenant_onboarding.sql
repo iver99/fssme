59d4da1155c8ef7edc21ee19444a8d16a61b951a
Vinay Joshi <vinay.joshi@oracle.com>
2015-08-27 05:10:29 -0700

    fixed primary key violation

--- savedsearch-schema/src/main/sql/emaas_tenant_onboarding.sql
+++ savedsearch-schema/src/main/sql/emaas_tenant_onboarding.sql
@@ -51,7 +51,6 @@ DECLARE
 @&EMSAAS_SQL_ROOT/1.0.0/emaas_savesearch_seed_data_la.sql  &TENANT_ID
 @&EMSAAS_SQL_ROOT/1.0.0/emaas_savesearch_seed_data_ta.sql  &TENANT_ID
 @&EMSAAS_SQL_ROOT/1.0.0/emaas_savesearch_seed_data_apm.sql  &TENANT_ID
-/
 
 BEGIN
 
@@ -83,10 +82,10 @@ END;
 /
 
 COMMIT;
-/
+
 
 @&EMSAAS_SQL_ROOT/1.0.20/emaas_savesearch_seed_data_ta.sql  &TENANT_ID
-/
+
 
 BEGIN
   DBMS_OUTPUT.PUT_LINE('Inserting OOB searches for &TENANT_ID is completed');
