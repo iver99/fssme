d6d55606d7f02d6f502e3105de4a0a32a54d8177
Guobao Chen <guobao.chen@oracle.com>
2015-12-22 00:58:55 -0800

    introduce sql upgrade files for R1.5.0

---
+++ savedsearch-schema/src/main/sql/1.5.0/emaas_savesearch_seed_data.sql
@@ -0,0 +1,23 @@
+Rem
+Rem emaas_savesearch_seed_data.sql
+Rem
+Rem Copyright (c) 2013, 2014, 2015, Oracle and/or its affiliates. 
+Rem All rights reserved.
+Rem
+Rem    NAME
+Rem      emaas_savesearch_seed_data.sql 
+Rem
+Rem    DESCRIPTION
+Rem      Seed data for category and default folder
+Rem
+Rem    NOTES
+Rem      None
+Rem
+Rem    MODIFIED   (MM/DD/YY)
+Rem    GUOCHEN     12/22/15 - Created
+Rem
+
+DEFINE TENANT_ID ='&1'
+SET FEEDBACK ON
+SET SERVEROUTPUT ON
+
---
+++ savedsearch-schema/src/main/sql/1.5.0/upgrade_impl_ddl.sql
@@ -0,0 +1,7 @@
+Rem
+Rem    DDL change during upgrade
+Rem    MODIFIED   (MM/DD/YY)
+Rem    GUOCHEN    12/22/2015 - created
+Rem
+Rem
+
