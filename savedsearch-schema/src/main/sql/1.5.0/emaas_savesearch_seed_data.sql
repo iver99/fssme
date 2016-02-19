3761ebd5e8f6c0d46c245344ef6e69ed628b347c
Miao Yu <miao.yu@oracle.com>
2016-02-19 18:17:09 +0800

    update service name for ITA from EmcitasApplications to emcitas-ui-apps

--- savedsearch-schema/src/main/sql/1.5.0/emaas_savesearch_seed_data.sql
+++ savedsearch-schema/src/main/sql/1.5.0/emaas_savesearch_seed_data.sql
@@ -112,3 +112,25 @@ EXCEPTION
     RAISE;
 END;
 /
+
+DECLARE
+  V_COUNT number;
+BEGIN
+
+SELECT COUNT(1) INTO V_COUNT from EMS_ANALYTICS_CATEGORY where TENANT_ID='&TENANT_ID' AND CATEGORY_ID=3;
+IF (V_COUNT>0) THEN
+  UPDATE EMS_ANALYTICS_CATEGORY SET PROVIDER_NAME='emcitas-ui-apps' WHERE TENANT_ID='&TENANT_ID' and CATEGORY_ID=3;
+  UPDATE EMS_ANALYTICS_SEARCH_PARAMS SET PARAM_VALUE_STR='emcitas-ui-apps' WHERE TENANT_ID='&TENANT_ID' and NAME='PROVIDER_NAME' and PARAM_VALUE_STR='EmcitasApplications';
+  COMMIT;
+  DBMS_OUTPUT.PUT_LINE('Provider name of ITA has been upgraded to emcitas-ui-apps for tenant: &TENANT_ID successfully! Upgraded records: '||V_COUNT);
+ELSE
+  DBMS_OUTPUT.PUT_LINE('Provider name of ITA has been upgraded to emcitas-ui-apps for tenant: &TENANT_ID before, no need to upgrade again');
+END IF;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    DBMS_OUTPUT.PUT_LINE('Failed to upgrade provider name of ITA to emcitas-ui-apps for tenant: &TENANT_ID due to '||SQLERRM);
+    RAISE;
+END;
+/
