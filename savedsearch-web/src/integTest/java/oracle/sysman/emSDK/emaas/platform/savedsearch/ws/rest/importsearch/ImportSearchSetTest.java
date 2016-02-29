25fdaf1f291169489e6b30a45ea610b6fd43b813
xiaoyuandai <xiaoyuan.dai@oracle.com>
2016-02-29 17:57:29 +0800

    unit test under savesearch-web

---
+++ savedsearch-web/src/integTest/java/oracle/sysman/emSDK/emaas/platform/savedsearch/ws/rest/importsearch/ImportSearchSetTest.java
@@ -0,0 +1,16 @@
+package oracle.sysman.emSDK.emaas.platform.savedsearch.ws.rest.importsearch;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by xidai on 2/29/2016.
+ */
+public class ImportSearchSetTest {
+
+    @Test
+    public void testImportSearches() throws Exception {
+
+    }
+}
