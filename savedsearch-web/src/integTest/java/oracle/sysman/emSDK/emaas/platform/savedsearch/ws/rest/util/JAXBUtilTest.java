9ae53c77748f5774363f1dace932042a1b7d10bb
xiaoyuandai <xiaoyuan.dai@oracle.com>
2016-03-09 15:50:12 +0800

    Unit Test Under WEB

--- savedsearch-web/src/integTest/java/oracle/sysman/emSDK/emaas/platform/savedsearch/ws/rest/util/JAXBUtilTest.java
+++ savedsearch-web/src/integTest/java/oracle/sysman/emSDK/emaas/platform/savedsearch/ws/rest/util/JAXBUtilTest.java
@@ -1,15 +1,32 @@
 package oracle.sysman.emSDK.emaas.platform.savedsearch.ws.rest.util;
 
+import mockit.Expectations;
+import mockit.Mocked;
 import org.eclipse.persistence.jaxb.xmlmodel.ObjectFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.UnmarshalException;
+import javax.xml.validation.SchemaFactory;
+import java.io.InputStream;
+import java.io.Reader;
 
 /**
  * Created by xidai on 2/26/2016.
  */
+@Test(groups={"s2"})
 public class JAXBUtilTest {
     private JAXBUtil jaxbUtil;
+    @Mocked
+    Reader reader;
+    @Mocked
+    InputStream inputStream;
+    @Mocked
+    JAXBContext jaxbContext;
+    @Mocked
+    SchemaFactory schemaFactory;
     @Test
     public void testGetJAXBContext() throws Exception {
         JAXBContext jaxbContext = JAXBUtil.getJAXBContext(ObjectFactory.class);
@@ -18,5 +35,40 @@ public class JAXBUtilTest {
     @Test
     public void testUnmarshal() throws Exception {
 
+        new Expectations(){
+            {
+                SchemaFactory.newInstance(anyString);
+                result = schemaFactory;
+            }
+        };
+        JAXBUtil.unmarshal(reader,inputStream,jaxbContext);
+    }
+
+    @Test
+    public void testUnmarshalFFor() throws Exception {
+
+        new Expectations(){
+            {
+                SchemaFactory.newInstance(anyString);
+                result = new UnmarshalException(new Throwable());
+            }
+        };
+        try {
+            JAXBUtil.unmarshal(reader, inputStream, jaxbContext);
+        }catch(Exception e){
+            Assert.assertTrue(true);}
+    }
+
+    @Test
+    public void testUnmarshalFForValidationEventHandler() throws Exception {
+        new Expectations() {
+            {
+                SchemaFactory.newInstance(anyString);
+                result = new JAXBException(new Throwable());
+            }
+        };
+        try {
+            JAXBUtil.unmarshal(reader, inputStream, jaxbContext);
+        }catch(Exception e){}
     }
 }
