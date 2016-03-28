50e1e861b32c77608be728b540c8e22e7e157214
qi.qian <qi.qian@oracle.com>
2016-03-28 11:35:32 +0800

    unit test for WidgetCacheRefreshServiceManager

---
+++ savedsearch-core/src/integTest/java/oracle/sysman/emSDK/emaas/platform/savedsearch/model/SearchManagerTest.java
@@ -0,0 +1,91 @@
+package oracle.sysman.emSDK.emaas.platform.savedsearch.model;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by QIQIAN on 2016/3/28.
+ */
+public class SearchManagerTest {
+
+    @Test
+    public void testGetInstance() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateNewSearch() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteSearch() throws Exception {
+
+    }
+
+    @Test
+    public void testEditSearch() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSearch() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSearchByName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSearchCountByFolderId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSearchListByCategoryId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSearchListByFolderId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSearchListByLastAccessDate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSystemSearchListByCategoryId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWidgetListByCategoryId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWidgetListByProviderNames() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWidgetScreenshotById() throws Exception {
+
+    }
+
+    @Test
+    public void testModifyLastAccessDate() throws Exception {
+
+    }
+
+    @Test
+    public void testSaveSearch() throws Exception {
+
+    }
+}
