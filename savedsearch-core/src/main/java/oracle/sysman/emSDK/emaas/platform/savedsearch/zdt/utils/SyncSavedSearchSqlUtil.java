c9174611f0f0ad7ff7cfccb6c62c490dbe47b38e
chehao <chendong.h.hao@oracle.com>
2016-07-29 11:36:23 +0800

    fix mistakes in sync data's SQL

--- savedsearch-core/src/main/java/oracle/sysman/emSDK/emaas/platform/savedsearch/zdt/utils/SyncSavedSearchSqlUtil.java
+++ savedsearch-core/src/main/java/oracle/sysman/emSDK/emaas/platform/savedsearch/zdt/utils/SyncSavedSearchSqlUtil.java
@@ -18,6 +18,7 @@ import java.math.BigInteger;
 public class SyncSavedSearchSqlUtil
 {
 
+	//	to_timestamp('" + lastModificationDate + "','yyyy-mm-dd hh24:mi:ss.ff')
 	public static String concatCategoryInsert(Long categoryId, String name, String description, String owner,
 			String creationDate, String nameNlsid, String nameSubSystem, String descriptionNlsid, String descriptionSubSystem,
 			String emPluginId, Long defaultFolderId, Long deleted, String providerName, String providerVersion,
@@ -25,10 +26,11 @@ public class SyncSavedSearchSqlUtil
 			String lastModificationDate)
 	{
 		String updateSql = "INSERT INTO EMS_ANALYTICS_CATEGORY VALUES(" + categoryId + ",'" + name + "','" + description + "','"
-				+ owner + "','" + creationDate + "','" + nameNlsid + "'," + "'" + nameSubSystem + "','" + descriptionNlsid
-				+ "','" + descriptionSubSystem + "','" + emPluginId + "'," + defaultFolderId + "," + deleted + ",'"
-				+ providerName + "'," + "'" + providerVersion + "','" + providerDiscovery + "','" + providerAssetroot + "',"
-				+ tenantId + ",'" + dashboardIneligible + "','" + lastModificationDate + "')";
+				+ owner + "',to_timestamp('" + creationDate + "','yyyy-mm-dd hh24:mi:ss.ff'),'" + nameNlsid + "'," + "'"
+				+ nameSubSystem + "','" + descriptionNlsid + "','" + descriptionSubSystem + "','" + emPluginId + "',"
+				+ defaultFolderId + "," + deleted + ",'" + providerName + "'," + "'" + providerVersion + "','"
+				+ providerDiscovery + "','" + providerAssetroot + "'," + tenantId + ",'" + dashboardIneligible
+				+ "',to_timestamp('" + lastModificationDate + "','yyyy-mm-dd hh24:mi:ss.ff'))";
 
 		return updateSql;
 	}
@@ -37,7 +39,8 @@ public class SyncSavedSearchSqlUtil
 			String creationDate, String lastModificationDate)
 	{
 		String insertSql = "INSERT INTO EMS_ANALYTICS_CATEGORY_PARAMS VALUES(" + categoryId + ",'" + name + "','" + paramValue
-				+ "'," + tenantId + "," + "'" + creationDate + "','" + lastModificationDate + "')";
+				+ "'," + tenantId + "," + "to_timestamp('" + creationDate + "','yyyy-mm-dd hh24:mi:ss.ff'),to_timestamp('"
+				+ lastModificationDate + "','yyyy-mm-dd hh24:mi:ss.ff'))";
 
 		return insertSql;
 	}
@@ -45,9 +48,11 @@ public class SyncSavedSearchSqlUtil
 	public static String concatCategoryParamUpdate(Long categoryId, String name, String paramValue, Long tenantId,
 			String creationDate, String lastModificationDate)
 	{
-		String insertSql = "UPDATE EMS_ANALYTICS_CATEGORY_PARAMS T SET T.PARAM_VALUE='" + paramValue + "',T.CREATION_DATE='"
-				+ creationDate + "'," + "T.LAST_MODIFICATION_DATE='" + lastModificationDate + "' where T.CATEGORY_ID="
-				+ categoryId + " and T.NAME='" + name + "' and T.TENANT_ID=" + tenantId;
+		String insertSql = "UPDATE EMS_ANALYTICS_CATEGORY_PARAMS T SET T.PARAM_VALUE='" + paramValue
+				+ "',T.CREATION_DATE=to_timestamp('" + creationDate + "','yyyy-mm-dd hh24:mi:ss.ff'),"
+				+ "T.LAST_MODIFICATION_DATE=to_timestamp('" + lastModificationDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff') where T.CATEGORY_ID=" + categoryId + " and T.NAME='" + name
+				+ "' and T.TENANT_ID=" + tenantId;
 
 		return insertSql;
 	}
@@ -59,12 +64,14 @@ public class SyncSavedSearchSqlUtil
 			String lastModificationDate)
 	{
 		String updateSql = "UPDATE EMS_ANALYTICS_CATEGORY t set t.NAME='" + name + "',t.DESCRIPTION='" + description
-				+ "',t.OWNER='" + owner + "'," + "t.CREATION_DATE='" + creationDate + "',t.NAME_NLSID='" + nameNlsid
-				+ "',t.NAME_SUBSYSTEM='" + nameSubSystem + "'," + "t.DESCRIPTION_NLSID='" + descriptionNlsid
-				+ "',t.DESCRIPTION_SUBSYSTEM='" + descriptionSubSystem + "',t.EM_PLUGIN_ID='" + emPluginId + "',"
-				+ "t.DEFAULT_FOLDER_ID=" + defaultFolderId + ",t.DELETED=" + deleted + ",t.PROVIDER_NAME='" + providerName
-				+ "',t.PROVIDER_VERSION='" + providerVersion + "'," + "t.PROVIDER_DISCOVERY='" + providerDiscovery
-				+ "',t.PROVIDER_ASSET_ROOT='" + providerAssetroot + "' where t.CATEGORY_ID=" + categoryId + " and t.TENANT_ID="
+				+ "',t.OWNER='" + owner + "'," + "t.CREATION_DATE=to_timestamp('" + creationDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff'),t.NAME_NLSID='" + nameNlsid + "',t.NAME_SUBSYSTEM='" + nameSubSystem + "',"
+				+ "t.DESCRIPTION_NLSID='" + descriptionNlsid + "',t.DESCRIPTION_SUBSYSTEM='" + descriptionSubSystem
+				+ "',t.EM_PLUGIN_ID='" + emPluginId + "'," + "t.DEFAULT_FOLDER_ID=" + defaultFolderId + ",t.DELETED=" + deleted
+				+ ",t.PROVIDER_NAME='" + providerName + "',t.PROVIDER_VERSION='" + providerVersion + "',"
+				+ "t.PROVIDER_DISCOVERY='" + providerDiscovery + "',t.PROVIDER_ASSET_ROOT='" + providerAssetroot
+				+ "',t.DASHBOARD_INELIGIBLE='" + dashboardIneligible + "',t.LAST_MODIFICATION_DATE=to_timestamp('"
+				+ lastModificationDate + "','yyyy-mm-dd hh24:mi:ss.ff') where t.CATEGORY_ID=" + categoryId + " and t.TENANT_ID="
 				+ tenantId;
 
 		return updateSql;
@@ -76,8 +83,9 @@ public class SyncSavedSearchSqlUtil
 			Long deleted, Long tenantId)
 	{
 		String insertSql = "INSERT INTO EMS_ANALYTICS_FOLDERS VALUES(" + folderId + ",'" + name + "'," + parentId + ",'"
-				+ description + "','" + creationDate + "'," + "'" + owner + "','" + lastModificationDate + "','" + lastModifiedBy
-				+ "','" + nameNlsid + "','" + nameSubsystem + "','" + descriptionNlsid + "','" + descriptionSubsystem + "',"
+				+ description + "',to_timestamp('" + creationDate + "','yyyy-mm-dd hh24:mi:ss.ff')," + "'" + owner
+				+ "',to_timestamp('" + lastModificationDate + "','yyyy-mm-dd hh24:mi:ss.ff'),'" + lastModifiedBy + "','"
+				+ nameNlsid + "','" + nameSubsystem + "','" + descriptionNlsid + "','" + descriptionSubsystem + "',"
 				+ systemFolder + ",'" + emPluginId + "'," + uiHidden + "," + deleted + "," + tenantId + ")";
 
 		return insertSql;
@@ -89,8 +97,9 @@ public class SyncSavedSearchSqlUtil
 			Long deleted, Long tenantId)
 	{
 		String updateSql = "UPDATE EMS_ANALYTICS_FOLDERS t SET t.NAME='" + name + "',t.PARENT_ID=" + parentId
-				+ ",t.DESCRIPTION='" + description + "'," + "t.CREATION_DATE='" + creationDate + "',t.OWNER='" + owner
-				+ "',t.LAST_MODIFICATION_DATE='" + lastModificationDate + "',t.LAST_MODIFIED_BY='" + lastModifiedBy + "',"
+				+ ",t.DESCRIPTION='" + description + "'," + "t.CREATION_DATE=to_timestamp('" + creationDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff'),t.OWNER='" + owner + "',t.LAST_MODIFICATION_DATE=to_timestamp('"
+				+ lastModificationDate + "','yyyy-mm-dd hh24:mi:ss.ff'),t.LAST_MODIFIED_BY='" + lastModifiedBy + "',"
 				+ "t.NAME_NLSID='" + nameNlsid + "',t.NAME_SUBSYSTEM='" + nameSubsystem + "',t.DESCRIPTION_NLSID='"
 				+ descriptionNlsid + "',t.DESCRIPTION_SUBSYSTEM='" + descriptionSubsystem + "'," + "t.SYSTEM_FOLDER="
 				+ systemFolder + ",t.EM_PLUGIN_ID='" + emPluginId + "',t.UI_HIDDEN=" + uiHidden + ",t.DELETED=" + deleted
@@ -103,7 +112,8 @@ public class SyncSavedSearchSqlUtil
 			Long tenantId, String creationDate, String lastModificationDate)
 	{
 		String insertSql = "INSERT INTO EMS_ANALYTICS_LAST_ACCESS VALUES(" + objectId + ",'" + accessedBy + "'," + objectType
-				+ ",'" + accessDate + "'," + tenantId + ",'" + creationDate + "','" + lastModificationDate + "')";
+				+ ",to_timestamp('" + accessDate + "','yyyy-mm-dd hh24:mi:ss.ff')," + tenantId + ",to_timestamp('" + creationDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff'),to_timestamp('" + lastModificationDate + "','yyyy-mm-dd hh24:mi:ss.ff'))";
 
 		return insertSql;
 	}
@@ -111,10 +121,11 @@ public class SyncSavedSearchSqlUtil
 	public static String concatLastAccessUpdate(Long objectId, String accessedBy, Long objectType, String accessDate,
 			Long tenantId, String creationDate, String lastModificationDate)
 	{
-		String updateSql = "UPDATE EMS_ANALYTICS_LAST_ACCESS t SET t.ACCESS_DATE='" + accessDate + "',t.CREATION_DATE='"
-				+ creationDate + "'," + "t.LAST_MODIFICATION_DATE='" + lastModificationDate + "' " + "where t.OBJECT_ID="
-				+ objectId + " and t.ACCESSED_BY='" + accessedBy + "' and t.OBJECT_TYPE=" + objectType + " and t.TENANT_ID="
-				+ tenantId + "";
+		String updateSql = "UPDATE EMS_ANALYTICS_LAST_ACCESS t SET t.ACCESS_DATE=to_timestamp('" + accessDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff'),t.CREATION_DATE=to_timestamp('" + creationDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff')," + "t.LAST_MODIFICATION_DATE=to_timestamp('" + lastModificationDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff') " + "where t.OBJECT_ID=" + objectId + " and t.ACCESSED_BY='" + accessedBy
+				+ "' and t.OBJECT_TYPE=" + objectType + " and t.TENANT_ID=" + tenantId + "";
 
 		return updateSql;
 	}
@@ -128,12 +139,13 @@ public class SyncSavedSearchSqlUtil
 			Long widgetLinkedDashboard, Long widgetDefaultWidth, Long widgetDefaultHeight, String dashboardIneligible,
 			String providerName, String providerVersion, String providerAssetRoot)
 	{
-		String insertSql = "INSERT INTO EMS_ANALYTICS_SEARCH VALUES(" + searchId + ",'" + searchGuid + "','" + name + "'.'"
-				+ owner + "','" + creationDate + "','" + lastModificationDate + "','" + lastModifiedBy + "'," + "'" + description
-				+ "'," + folderId + "," + categoryId + ",'" + nameNlsid + "','" + nameSubsystem + "','" + descriptionNlsid
-				+ "','" + descriptionSubsystem + "'," + systemSearch + ",'" + emPluginId + "'," + isLocked + ",'" + metaDataClob
-				+ "','" + searchDisplayStr + "'," + "" + uiHidden + "," + deleted + "," + isWidget + "," + tenantId + ",'"
-				+ nameWidgetSource + "','" + widgetGroupName + "','" + widgetScreenshotHref + "',''" + widgetIcon + "','"
+		String insertSql = "INSERT INTO EMS_ANALYTICS_SEARCH VALUES(" + searchId + ",'" + searchGuid + "','" + name + "','"
+				+ owner + "',to_timestamp('" + creationDate + "','yyyy-mm-dd hh24:mi:ss.ff'),to_timestamp('"
+				+ lastModificationDate + "','yyyy-mm-dd hh24:mi:ss.ff'),'" + lastModifiedBy + "'," + "'" + description + "',"
+				+ folderId + "," + categoryId + ",'" + nameNlsid + "','" + nameSubsystem + "','" + descriptionNlsid + "','"
+				+ descriptionSubsystem + "'," + systemSearch + ",'" + emPluginId + "'," + isLocked + ",'" + metaDataClob + "','"
+				+ searchDisplayStr + "'," + "" + uiHidden + "," + deleted + "," + isWidget + "," + tenantId + ",'"
+				+ nameWidgetSource + "','" + widgetGroupName + "','" + widgetScreenshotHref + "','" + widgetIcon + "','"
 				+ widgetKocName + "','" + viewModel + "','" + widgetTemplate + "'," + "'" + widgetSupportTimeControl + "',"
 				+ widgetLinkedDashboard + "," + widgetDefaultWidth + "," + widgetDefaultHeight + ",'" + providerName + "','"
 				+ providerVersion + "','" + providerAssetRoot + "','" + dashboardIneligible + "')";//look out the order of 3 provider* columns and dashboard_ineligible
@@ -145,8 +157,9 @@ public class SyncSavedSearchSqlUtil
 			String paramValueStr, String paramValueClob, Long tenantId, String creationDate, String lastModificationDate)
 	{
 		String insertSql = "INSERT INTO EMS_ANALYTICS_SEARCH_PARAMS VALUES(" + searchId + ",'" + name + "','" + paramAttributes
-				+ "'," + paramType + "," + "'" + paramValueStr + "','" + paramValueClob + "'," + tenantId + ",'" + creationDate
-				+ "','" + lastModificationDate + "')";
+				+ "'," + paramType + "," + "'" + paramValueStr + "','" + paramValueClob + "'," + tenantId + ",to_timestamp('"
+				+ creationDate + "','yyyy-mm-dd hh24:mi:ss.ff'),to_timestamp('" + lastModificationDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff'))";
 
 		return insertSql;
 	}
@@ -156,8 +169,10 @@ public class SyncSavedSearchSqlUtil
 	{
 		String updateSql = "UPDATE EMS_ANALYTICS_SEARCH_PARAMS t set t.PARAM_ATTRIBUTES='" + paramAttributes + "',t.PARAM_TYPE="
 				+ paramType + "," + "t.PARAM_VALUE_STR='" + paramValueStr + "',t.PARAM_VALUE_CLOB='" + paramValueClob
-				+ "',t.CREATION_DATE='" + creationDate + "'," + "t.LAST_MODIFICATION_DATE='" + lastModificationDate
-				+ "' where t.SEARCH_ID=" + searchId + " and t.NAME='" + name + "' and t.TENANT_ID=" + tenantId + "";
+				+ "',t.CREATION_DATE=to_timestamp('" + creationDate + "','yyyy-mm-dd hh24:mi:ss.ff'),"
+				+ "t.LAST_MODIFICATION_DATE=to_timestamp('" + lastModificationDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff') where t.SEARCH_ID=" + searchId + " and t.NAME='" + name + "' and t.TENANT_ID="
+				+ tenantId + "";
 
 		return updateSql;
 	}
@@ -171,12 +186,13 @@ public class SyncSavedSearchSqlUtil
 			Long widgetLinkedDashboard, Long widgetDefaultWidth, Long widgetDefaultHeight, String dashboardIneligible,
 			String providerName, String providerVersion, String providerAssetRoot)
 	{
-		String updateSql = "UPDATE EMS_ANALYTICS_SEARCH_PARAMS t set t.SEARCH_GUID='" + searchGuid + "',t.NAME='" + name
-				+ "',t.OWNER='" + owner + "'," + "t.CREATION_DATE='" + creationDate + "',t.LAST_MODIFICATION_DATE='"
-				+ lastModificationDate + "',t.LAST_MODIFIED_BY='" + lastModifiedBy + "'," + "t.DESCRIPTION='" + description
+		String updateSql = "UPDATE EMS_ANALYTICS_SEARCH t set t.SEARCH_GUID='" + searchGuid + "',t.NAME='" + name + "',t.OWNER='"
+				+ owner + "'," + "t.CREATION_DATE=to_timestamp('" + creationDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff'),t.LAST_MODIFICATION_DATE=to_timestamp('" + lastModificationDate
+				+ "','yyyy-mm-dd hh24:mi:ss.ff'),t.LAST_MODIFIED_BY='" + lastModifiedBy + "'," + "t.DESCRIPTION='" + description
 				+ "',t.FOLDER_ID=" + folderId + ",t.CATEGORY_ID=" + categoryId + ",t.NAME_NLSID='" + nameNlsid
 				+ "',t.NAME_SUBSYSTEM='" + nameSubsystem + "'," + "t.DESCRIPTION_NLSID='" + descriptionNlsid
-				+ "',t.DESCRIPTION_SUBSSYSTEM='" + descriptionSubsystem + "',t.SYSTEM_SEARCH=" + systemSearch
+				+ "',t.DESCRIPTION_SUBSYSTEM='" + descriptionSubsystem + "',t.SYSTEM_SEARCH=" + systemSearch
 				+ ",t.EM_PLUGIN_ID='" + systemSearch + "'," + "t.IS_LOCKED=" + isLocked + ",t.METADATA_CLOB='" + metaDataClob
 				+ "',t.SEARCH_DISPLAY_STR='" + searchDisplayStr + "',t.UI_HIDDEN=" + uiHidden + ",t.DELETED=" + deleted + ","
 				+ "t.IS_WIDGET=" + isWidget + ",t.NAME_WIDGET_SOURCE='" + nameWidgetSource + "',t.WIDGET_GROUP_NAME='"
