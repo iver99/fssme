111d207288799d0f8d836f4125b665f58772ebad
xia_dai <xiaoyuan.d.dai@oracle.com>
2016-09-05 16:32:47 +0800

    EMCPSSF-354: Check the OOB param not null

---
+++ savedsearch-core/src/integTest/java/oracle/sysman/emaas/savedsearch/OOBWidgetTest.java
@@ -0,0 +1,41 @@
+package oracle.sysman.emaas.savedsearch;
+
+import oracle.sysman.SDKImpl.emaas.platform.savedsearch.persistence.QAToolUtil;
+import oracle.sysman.emSDK.emaas.platform.savedsearch.exception.EMAnalyticsFwkException;
+import oracle.sysman.emSDK.emaas.platform.savedsearch.model.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+@Test
+public class OOBWidgetTest {
+    @Test
+    public void testOOBWidgetParamNotNull() throws EMAnalyticsFwkException {
+        TenantContext.setContext(new TenantInfo(
+                TestUtils.getUsername(QAToolUtil.getTenantDetails().get(QAToolUtil.TENANT_USER_NAME).toString()),
+                TestUtils.getInternalTenantId(QAToolUtil.getTenantDetails().get(QAToolUtil.TENANT_NAME).toString())));
+        try {
+            CategoryManager categoryManager = CategoryManager.getInstance();
+            SearchManager searchManager = SearchManager.getInstance();
+            List<Category> categoryList = categoryManager.getAllCategories();
+            for (Category category : categoryList) {
+                List<Search> searchList = searchManager.getSearchListByCategoryId(category.getId());
+                for (Search search : searchList) {
+                    boolean isSystemSearch = search.isSystemSearch();
+                    boolean isWidget = search.getIsWidget();
+                    if (isSystemSearch && isWidget) {
+                        for(SearchParameter searchParam : search.getParameters()){
+                            if("WIDGET_KOC_NAME".equals(searchParam.getName())||"WIDGET_VIEWMODEL".equals(searchParam.getName())||"WIDGET_TEMPLATE".equals(searchParam.getName())){
+                                Assert.assertNotNull(searchParam.getValue());
+                            }
+                        }
+                    }
+                }
+            }
+        }finally{
+            TenantContext.clearContext();
+        }
+    }
+
+}
