b9b3e0bf7a1c7902bd3bf9e506e3c63b1d9ebbae
rex.liang <rex.liang@oracle.com>
2016-10-08 02:15:44 -0700

    update version to 1.12.0 and put zdt sql to 1.12.0

---
+++ savedsearch-schema/src/main/sql/1.12.0/schema_ops.sql
@@ -0,0 +1,16 @@
+Rem ----------------------------------------------------------------
+Rem Upgrade from 1.0.0 (this first version with multiple tenancy support) to latest
+Rem Note:
+Rem   Upgrade from any version below 1.0.0 is not supported any more.
+Rem
+Rem 07/05/2016	ADUAN	    created
+Rem ----------------------------------------------------------------
+
+@&EMSAAS_SQL_ROOT/1.12.0/upgrade_impl_ddl.sql
+
+SET HEADING OFF
+SET FEEDBACK OFF
+SET LINESIZE 2000
+
+@&EMSAAS_SQL_ROOT/1.12.0/upgrade_impl_dml.sql
+
