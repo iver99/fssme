f18c454edb44b6cd8af5023f41c5d35c874dbba2
chehao <chendong.h.hao@oracle.com>
2016-10-25 17:16:03 +0800

    ajust server cache policy to 30 mins expiration

--- savedsearch-core/src/main/java/oracle/sysman/emSDK/emaas/platform/savedsearch/cache/lru/CacheFactory.java
+++ savedsearch-core/src/main/java/oracle/sysman/emSDK/emaas/platform/savedsearch/cache/lru/CacheFactory.java
@@ -1,6 +1,7 @@
 package oracle.sysman.emSDK.emaas.platform.savedsearch.cache.lru;
 
 import java.util.Map;
+import java.util.ResourceBundle;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.logging.log4j.LogManager;
@@ -11,18 +12,21 @@ public class CacheFactory {
 	
 	private static final Logger LOGGER = LogManager.getLogger(CacheFactory.class);
 	private static final int DEFAULT_EXPIRE_TIME=0;
+	private static final int DEFAULT_CACHE_UNIT_CAPACITY = Integer.valueOf(ResourceBundle.getBundle("cache_config").getString(
+			"DEFAULT_CACHE_UNIT_CAPACITY"));
 	
 	private static Map<String,CacheUnit> cacheUnitMap=new ConcurrentHashMap<String,CacheUnit>();
 	
 	
 	public static CacheUnit getCache(String cacheName){
-		return getCache(cacheName,DEFAULT_EXPIRE_TIME);
+		return getCache(cacheName,DEFAULT_CACHE_UNIT_CAPACITY,DEFAULT_EXPIRE_TIME);
 	}
 
 	private CacheFactory() {
 	}
 
-	public static CacheUnit getCache(String cacheName, int timeToLive){
+	public static CacheUnit getCache(String cacheName, int capacity, int timeToLive)
+	{
 		if(cacheName ==null) {
 			LOGGER.error("CacheFactory:Cache name cannot be null!");
 			throw new IllegalArgumentException("cache name cannot be null!");
@@ -33,15 +37,16 @@ public class CacheFactory {
 		}
 		CacheUnit cu=cacheUnitMap.get(cacheName);
 		if(cu == null){
-			return createCacheUnit(cacheName, timeToLive);
+			return createCacheUnit(cacheName, capacity, timeToLive);
 		}else{
 			return cu;
 		}
 		
 	}
 	
-	private static CacheUnit createCacheUnit(String cacheName,int timeToLive){
-		CacheUnit cu=new CacheUnit(cacheName,timeToLive);
+	private static CacheUnit createCacheUnit(String cacheName, int capacity, int timeToLive)
+	{
+		CacheUnit cu = new CacheUnit(cacheName, capacity, timeToLive);
 		cacheUnitMap.put(cacheName, cu);
 		LOGGER.debug("CacheFactory:Cache named: {},time to live: {} has been created.", cacheName, timeToLive);
 		return cu;
