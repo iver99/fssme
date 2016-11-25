15c115cf2b9b4fd683dc7487f58cc6cd457ecc16
xia_dai <xiaoyuan.d.dai@oracle.com>
2016-11-25 11:31:08 +0800

    EMCPSSF419. remove the added widget

--- savedsearch-schema/src/main/sql/1.13.0/emaas_savesearch_seed_data_targetcard.sql
+++ savedsearch-schema/src/main/sql/1.13.0/emaas_savesearch_seed_data_targetcard.sql
@@ -26,29 +26,11 @@ DECLARE
     V_COUNT NUMBER;
     V_SEARCH_ID                         NUMBER(38,0)         ;
     V_NAME                              VARCHAR2(64 BYTE)    ;
-    V_OWNER                             VARCHAR2(64 BYTE)    ;
-    V_CREATION_DATE                     TIMESTAMP(6)         ;
-    V_LAST_MODIFICATION_DATE            TIMESTAMP(6)         ;
-    V_LAST_MODIFIED_BY                  VARCHAR2(64 BYTE)    ;
-    V_DESCRIPTION                       VARCHAR2(256 BYTE)   ;
-    V_FOLDER_ID                         NUMBER(38,0)         ;
-    V_CATEGORY_ID                       NUMBER(38,0)         ;
-    V_SYSTEM_SEARCH                     NUMBER(1,0)          ;
-    V_IS_LOCKED                         NUMBER(1,0)          ;
-    V_UI_HIDDEN                         NUMBER(1,0)          ;
-    V_DELETED                           NUMBER(38,0)         ;
-    V_IS_WIDGET                         NUMBER(1,0)          ;
     V_TENANT_ID                         NUMBER(38,0)         ;
-    V_PARAM_ATTRIBUTES                  EMS_ANALYTICS_SEARCH_PARAMS.PARAM_ATTRIBUTES%TYPE;
     V_PARAM_TYPE                        EMS_ANALYTICS_SEARCH_PARAMS.PARAM_TYPE%TYPE;
     V_PARAM_VALUE_STR                   EMS_ANALYTICS_SEARCH_PARAMS.PARAM_VALUE_STR%TYPE;
     V_PARAM_VALUE_CLOB                  CLOB                           ;
-    V_CLOB                              CLOB                           ;
-    V_SEARCH_STR                        CLOB                           ;
-    V_METADATA                          CLOB                           ;
-    V_LAST_ACCESS_DATE                  TIMESTAMP(6)                   ;
     V_TID                               NUMBER(38,0) := '&TENANT_ID'   ;
-    V_OBJECT_TYPE                       NUMBER(1,0)                    ;
     CURSOR TENANT_CURSOR IS
        SELECT DISTINCT TENANT_ID FROM EMS_ANALYTICS_FOLDERS ORDER BY TENANT_ID;
 
@@ -61,83 +43,6 @@ BEGIN
        FETCH TENANT_CURSOR INTO V_TENANT_ID;
        EXIT WHEN TENANT_CURSOR%NOTFOUND;
      END IF;
-
-    V_SEARCH_ID := 4010;
-    SELECT COUNT(1) INTO V_COUNT FROM EMS_ANALYTICS_SEARCH WHERE SEARCH_ID = V_SEARCH_ID AND TENANT_ID  = V_TENANT_ID;
-    IF ( V_COUNT = 0) THEN
-        V_NAME                     :='CARD_DEF_omc_hosted_target';
-        V_OWNER                    :='ORACLE';
-        V_CREATION_DATE            :=SYS_EXTRACT_UTC(SYSTIMESTAMP);
-        V_LAST_MODIFICATION_DATE   :=SYS_EXTRACT_UTC(SYSTIMESTAMP);
-        V_LAST_MODIFIED_BY         :='ORACLE';
-        V_DESCRIPTION              :=null;
-        V_FOLDER_ID                := 6;
-        V_CATEGORY_ID              :=5;
-        V_SYSTEM_SEARCH            :=1;
-        V_IS_LOCKED                :=0;
-        V_UI_HIDDEN                :=0;
-        V_IS_WIDGET                :=0;
-        INSERT INTO EMS_ANALYTICS_SEARCH
-            (SEARCH_ID, NAME, OWNER, CREATION_DATE, LAST_MODIFICATION_DATE,
-                LAST_MODIFIED_BY, DESCRIPTION, FOLDER_ID, CATEGORY_ID, SYSTEM_SEARCH,
-                IS_LOCKED, UI_HIDDEN, TENANT_ID, IS_WIDGET)
-        VALUES
-            (V_SEARCH_ID, V_NAME, V_OWNER, V_CREATION_DATE, V_LAST_MODIFICATION_DATE,
-                V_LAST_MODIFIED_BY, V_DESCRIPTION, V_FOLDER_ID, V_CATEGORY_ID, V_SYSTEM_SEARCH,
-                V_IS_LOCKED, V_UI_HIDDEN, V_TENANT_ID, V_IS_WIDGET);
-
-        V_NAME                  :='cardef';
-        V_PARAM_ATTRIBUTES      :=null;
-        V_PARAM_TYPE            :=2;
-        V_PARAM_VALUE_STR       :=null;
-        V_PARAM_VALUE_CLOB      :='{"id":null,"entityType":"omc_hosted_target","widgets":null,"widgetRefs":[{"id":3031,"parameters":null,"entityCapability":null,"accessControl":{"userPrivilege":"USE_TARGET_ANALYTICS"}}]}';
-        INSERT INTO EMS_ANALYTICS_SEARCH_PARAMS
-            (SEARCH_ID,NAME,PARAM_ATTRIBUTES,PARAM_TYPE,PARAM_VALUE_STR,PARAM_VALUE_CLOB,TENANT_ID)
-        VALUES
-            (V_SEARCH_ID, V_NAME, V_PARAM_ATTRIBUTES, V_PARAM_TYPE, V_PARAM_VALUE_STR, V_PARAM_VALUE_CLOB, V_TENANT_ID);
-
-        V_NAME                  :='entityType';
-        V_PARAM_ATTRIBUTES      :=null;
-        V_PARAM_TYPE            :=1;
-        V_PARAM_VALUE_STR       :='omc_hosted_target';
-        V_PARAM_VALUE_CLOB      :=null;
-        INSERT INTO EMS_ANALYTICS_SEARCH_PARAMS
-            (SEARCH_ID,NAME,PARAM_ATTRIBUTES,PARAM_TYPE,PARAM_VALUE_STR,PARAM_VALUE_CLOB,TENANT_ID)
-        VALUES
-            (V_SEARCH_ID, V_NAME, V_PARAM_ATTRIBUTES, V_PARAM_TYPE, V_PARAM_VALUE_STR, V_PARAM_VALUE_CLOB, V_TENANT_ID);
-
-        V_NAME                  :='isCardDef';
-        V_PARAM_ATTRIBUTES      :=null;
-        V_PARAM_TYPE            :=1;
-        V_PARAM_VALUE_STR       :='true';
-        V_PARAM_VALUE_CLOB      :=null;
-        INSERT INTO EMS_ANALYTICS_SEARCH_PARAMS
-            (SEARCH_ID,NAME,PARAM_ATTRIBUTES,PARAM_TYPE,PARAM_VALUE_STR,PARAM_VALUE_CLOB,TENANT_ID)
-        VALUES
-            (V_SEARCH_ID, V_NAME, V_PARAM_ATTRIBUTES, V_PARAM_TYPE, V_PARAM_VALUE_STR, V_PARAM_VALUE_CLOB, V_TENANT_ID);
-
-        V_NAME                  :='version';
-        V_PARAM_ATTRIBUTES      :=null;
-        V_PARAM_TYPE            :=1;
-        V_PARAM_VALUE_STR       :='1001';
-        V_PARAM_VALUE_CLOB      :=null;
-        INSERT INTO EMS_ANALYTICS_SEARCH_PARAMS
-            (SEARCH_ID,NAME,PARAM_ATTRIBUTES,PARAM_TYPE,PARAM_VALUE_STR,PARAM_VALUE_CLOB,TENANT_ID)
-        VALUES
-            (V_SEARCH_ID, V_NAME, V_PARAM_ATTRIBUTES, V_PARAM_TYPE, V_PARAM_VALUE_STR, V_PARAM_VALUE_CLOB, V_TENANT_ID);
-
-
-        V_LAST_ACCESS_DATE      :=SYS_EXTRACT_UTC(SYSTIMESTAMP);
-        V_OBJECT_TYPE           :=2;
-        INSERT INTO EMS_ANALYTICS_LAST_ACCESS
-            (OBJECT_ID, ACCESSED_BY, OBJECT_TYPE, ACCESS_DATE, TENANT_ID)
-        VALUES
-            (V_SEARCH_ID, V_OWNER, V_OBJECT_TYPE, V_LAST_ACCESS_DATE, V_TENANT_ID);
-
-        DBMS_OUTPUT.PUT_LINE('OOB Target Card mapping (id='||V_SEARCH_ID||') is added');
-    ELSE
-        DBMS_OUTPUT.PUT_LINE('OOB Target Card mapping (id='||V_SEARCH_ID||') exists, no need to add again');
-    END IF;  
 
     V_SEARCH_ID       := 4000;
     V_NAME            := 'CARD_DEF_omc_web_application_server';
