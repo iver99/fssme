01a32e008b2eda8141cfe53261234727186d6015
xia_dai <xiaoyuan.d.dai@oracle.com>
2016-11-28 16:10:30 +0800

    EMCPDF-2716 Integration to include new umbrella widget

--- savedsearch-schema/src/main/sql/1.13.0/upgrade_impl_dml.sql
+++ savedsearch-schema/src/main/sql/1.13.0/upgrade_impl_dml.sql
@@ -5,5 +5,6 @@ Rem
 
 @&EMSAAS_SQL_ROOT/1.13.0/emaas_savesearch_seed_data.sql -1
 @&EMSAAS_SQL_ROOT/1.13.0/emaas_savesearch_seed_data_targetcard.sql -1
+@&EMSAAS_SQL_ROOT/1.13.0/emaas_savesearch_cos_widget_update.sql -1
 COMMIT;
 
