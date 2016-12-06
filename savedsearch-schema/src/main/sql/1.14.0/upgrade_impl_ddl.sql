a0ee3b0705968e6721218de1a8ffdb6f1b5113b1
xia_dai <xiaoyuan.d.dai@oracle.com>
2016-12-06 15:15:58 +0800

    For the orchestration 1.14 landing

---
+++ savedsearch-schema/src/main/sql/1.14.0/upgrade_impl_ddl.sql
@@ -0,0 +1,6 @@
+Rem
+Rem    DDL change during upgrade
+Rem    MODIFIED   (MM/DD/YY)
+Rem    xiadai     12/06/2016
+
+
---
+++ savedsearch-schema/src/main/sql/1.14.0/upgrade_impl_dml.sql
@@ -0,0 +1,8 @@
+Rem
+Rem    MODIFIED   (MM/DD/YY)
+Rem    REX      12/06/2016 - created
+Rem
+
+@&EMSAAS_SQL_ROOT/1.14.0/emaas_savesearch_cos_widget_update.sql -1
+COMMIT;
+
