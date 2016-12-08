ed10fdda4ecf5f5db86e43f3989d1aed97dd1a5a
xia_dai <xiaoyuan.d.dai@oracle.com>
2016-12-08 15:54:22 +0800

    no message

--- savedsearch-schema/src/main/sql/1.13.0/upgrade_impl_dml.sql
+++ savedsearch-schema/src/main/sql/1.13.0/upgrade_impl_dml.sql
@@ -5,6 +5,5 @@ Rem
 
 @&EMSAAS_SQL_ROOT/1.13.0/emaas_savesearch_seed_data.sql -1
 @&EMSAAS_SQL_ROOT/1.13.0/emaas_savesearch_seed_data_targetcard.sql -1
-@&EMSAAS_SQL_ROOT/1.13.0/emaas_savesearch_cos_widget_update.sql -1
 COMMIT;
 
