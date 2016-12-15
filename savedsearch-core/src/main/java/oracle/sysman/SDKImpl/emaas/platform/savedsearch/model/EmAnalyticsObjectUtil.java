622a618f6b0500c4c60e0fcbca5a5bca06526232
rex.liang <rex.liang@oracle.com>
2016-12-14 22:00:02 -0800

    EMCPSSF-435: avoid to edit the owner of ss

--- savedsearch-core/src/main/java/oracle/sysman/SDKImpl/emaas/platform/savedsearch/model/EmAnalyticsObjectUtil.java
+++ savedsearch-core/src/main/java/oracle/sysman/SDKImpl/emaas/platform/savedsearch/model/EmAnalyticsObjectUtil.java
@@ -517,7 +517,6 @@ class EmAnalyticsObjectUtil
 		Date utcNow = DateUtil.getCurrentUTCTime();
 		if (!RequestType.INTERNAL_TENANT.equals(RequestContext.getContext())) {
 			String currentUser = ExecutionContext.getExecutionContext().getCurrentUser();
-			searchEntity.setOwner(currentUser);
 			searchEntity.setLastModifiedBy(currentUser);
 		}
 		searchEntity.setLastModificationDate(utcNow);
