2714e2c5dc7b690a3854b4d0ed3f775c3acf566d
xia_dai <xiaoyuan.d.dai@oracle.com>
2016-12-20 17:48:54 +0800

    add 1.14.0/schema_ops.sql

---
+++ savedsearch-schema/src/main/sql/1.14.0/schema_ops.sql
@@ -0,0 +1,13 @@
+Rem ----------------------------------------------------------------
+Rem
+Rem 12/20/2016	XIADAI	    created
+Rem ----------------------------------------------------------------
+
+@&EMSAAS_SQL_ROOT/1.14.0/upgrade_impl_ddl.sql
+
+SET HEADING OFF
+SET FEEDBACK OFF
+SET LINESIZE 2000
+
+@&EMSAAS_SQL_ROOT/1.14.0/upgrade_impl_dml.sql
+
---
+++ savedsearch-schema/src/main/sql/1.14.0/upgrade_impl_ddl.sql
@@ -0,0 +1,5 @@
+Rem
+Rem    DDL change during upgrade
+Rem    MODIFIED   (MM/DD/YY)
+
+
