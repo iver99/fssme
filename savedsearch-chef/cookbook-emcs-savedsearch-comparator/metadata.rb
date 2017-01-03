ca16b19aff86f655f8957fb18aa2aa31f57c1d14
ping.w.wu@oracle.com <pingwu@slc09wob.us.oracle.com>
2017-01-03 05:31:28 -0800

    add softwarejson and cookbook for ssf comparator

---
+++ savedsearch-chef/cookbook-emcs-savedsearch-comparator/metadata.rb
@@ -0,0 +1,15 @@
+name             'cookbook-emcs-savedsearch-comparator'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures datasource'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.3.0'
+depends "cookbook-emcs-emsaas-weblogic"
+
+#The schema operations are performed with recipes from the cook book -  'cookbook-emcs-lcm-rep-manager-service'  adding  the following dependency to metadata.rb
+
+depends "cookbook-emcs-lcm-rep-manager-service"
+ 
+depends "cookbook-emcs-dbconfig-12102-0"
+
