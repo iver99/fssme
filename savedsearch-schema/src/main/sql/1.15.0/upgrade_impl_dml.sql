6d44d3755330da404ace62c122505c6af876f927
rex.liang <rex.liang@oracle.com>
2017-01-08 23:06:00 -0800

    upgrade version to 1.15 and move the related SQL into 1.15.0 folder

--- savedsearch-schema/src/main/sql/1.15.0/upgrade_impl_dml.sql
+++ savedsearch-schema/src/main/sql/1.15.0/upgrade_impl_dml.sql
@@ -4,5 +4,6 @@ Rem    REX      01/04/2017 - created
 Rem
 
 @&EMSAAS_SQL_ROOT/1.15.0/emaas_savesearch_seed_data_ta.sql -1
+@&EMSAAS_SQL_ROOT/1.15.0/emaas_savedsearch_cos_widget_update.sql -1
 COMMIT;
 
