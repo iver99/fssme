234ddd45376b07d52ecb020ae7d89f0e17dcec19
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-02-16 11:10:55 +0800

    EMCPSSF-462

--- savedsearch-schema/src/main/sql/emaas_tenant_onboarding.sql
+++ savedsearch-schema/src/main/sql/emaas_tenant_onboarding.sql
@@ -140,7 +140,7 @@ COMMIT;
 @&EMSAAS_SQL_ROOT/1.15.0/emaas_savedsearch_seed_data_cos.sql &TENANT_ID
 @&EMSAAS_SQL_ROOT/1.15.0/emaas_savedsearch_seed_data_la.sql &TENANT_ID
 
-@&EMSAAS_SQL_ROOT/1.16.0/emaas_savedsearch_seed_data_os.sql &TENANT_ID
+@&EMSAAS_SQL_ROOT/1.16.0/emaas_savedsearch_seed_data_cos.sql &TENANT_ID
 
 BEGIN
   DBMS_OUTPUT.PUT_LINE('Inserting OOB searches for &TENANT_ID is completed');
