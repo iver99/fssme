e43da1be3babec8c9fea0065fee5dc69a0b3ecda
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-02-28 16:42:55 +0800

    EMCPSSF-462

--- savedsearch-schema/src/main/sql/1.17.0/upgrade_impl_dml.sql
+++ savedsearch-schema/src/main/sql/1.17.0/upgrade_impl_dml.sql
@@ -4,6 +4,6 @@ Rem    XIADAI      02/06/2017 - created
 Rem
 
 
-@&EMSAAS_SQL_ROOT/1.16.0/emaas_savedsearch_seed_data_cos.sql -1
+@&EMSAAS_SQL_ROOT/1.17.0/emaas_savedsearch_seed_data_cos.sql -1
 COMMIT;
 
