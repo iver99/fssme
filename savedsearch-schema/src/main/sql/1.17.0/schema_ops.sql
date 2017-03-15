d35339a6e9cc1840342f456dd094ce120d073ef0
rex.liang <rex.liang@oracle.com>
2017-03-15 00:48:47 -0700

    EMCPSSF-465: upgrade sql's version from 1.16 to 1.17

--- savedsearch-schema/src/main/sql/1.17.0/schema_ops.sql
+++ savedsearch-schema/src/main/sql/1.17.0/schema_ops.sql
@@ -3,11 +3,11 @@ Rem
 Rem 02/12/2017	MIAYU	    created
 Rem ----------------------------------------------------------------
 
-@&EMSAAS_SQL_ROOT/1.16.0/upgrade_impl_ddl.sql
+@&EMSAAS_SQL_ROOT/1.17.0/upgrade_impl_ddl.sql
 
 SET HEADING OFF
 SET FEEDBACK OFF
 SET LINESIZE 2000
 
-@&EMSAAS_SQL_ROOT/1.16.0/upgrade_impl_dml.sql
+@&EMSAAS_SQL_ROOT/1.17.0/upgrade_impl_dml.sql
 
