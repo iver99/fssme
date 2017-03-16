6f16a5f50c2e2643e1036c9e395d2735eb695fee
chehao <chendong.h.hao@oracle.com>
2017-03-16 09:48:54 +0800

    revert compress table of search table emcpssf470

--- savedsearch-schema/src/main/sql/1.17.0/upgrade_impl_ddl.sql
+++
@@ -1,8 +0,0 @@
-Rem
-Rem    DDL change during upgrade
-Rem    MODIFIED   (MM/DD/YY)
-Rem    CHEHAO    03/07/2017 - created
-Rem
-Rem
-
-@&EMSAAS_SQL_ROOT/1.17.0/emaas_savesearch_compress_search_table.sql
--- savedsearch-schema/src/main/sql/1.17.0/upgrade_impl_dml.sql
+++
@@ -1,4 +0,0 @@
-Rem
-Rem    MODIFIED   (MM/DD/YY)
-Rem    Chehao      03/07/2017 - created
-Rem
