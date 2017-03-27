b49162462cb7b6bd2184b2a8987a178ac521faaa
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-03-27 17:32:48 +0800

    EMCPSSF-483

---
+++ savedsearch-schema/src/main/sql/1.18.0/upgrade_impl_ddl.sql
@@ -0,0 +1,4 @@
+Rem
+Rem    DDL change during upgrade
+Rem    MODIFIED   (MM/DD/YY)
+Rem    xiadai   02/11/2017
---
+++ savedsearch-schema/src/main/sql/1.18.0/upgrade_impl_dml.sql
@@ -0,0 +1,10 @@
+Rem
+Rem    MODIFIED   (MM/DD/YY)
+Rem    xiadai      03/27/2017 - created
+Rem
+@&EMSAAS_SQL_ROOT/1.18.0/emaas_savedsearch_seed_data_cos.sql -1
+
+
+COMMIT;
+
+
