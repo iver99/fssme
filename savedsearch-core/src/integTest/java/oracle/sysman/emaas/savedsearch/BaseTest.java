5a2d4914c95b2d525763b6776488bac19d6b8e54
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-05-27 16:10:21 +0800

    no message

--- savedsearch-core/src/integTest/java/oracle/sysman/emaas/savedsearch/BaseTest.java
+++ savedsearch-core/src/integTest/java/oracle/sysman/emaas/savedsearch/BaseTest.java
@@ -20,17 +20,11 @@ public abstract class BaseTest
 		
 		// set db env to system property for running test cases
 		Properties props = QAToolUtil.getDbProperties();
-//		if (props == null) {
-//			System.setProperty(PersistenceManager.JDBC_PARAM_URL, props.getProperty(QAToolUtil.JDBC_PARAM_URL));
-//			System.setProperty(PersistenceManager.JDBC_PARAM_USER, props.getProperty(QAToolUtil.JDBC_PARAM_USER));
-//			System.setProperty(PersistenceManager.JDBC_PARAM_PASSWORD, props.getProperty(QAToolUtil.JDBC_PARAM_PASSWORD));
-//			System.setProperty(PersistenceManager.JDBC_PARAM_DRIVER, props.getProperty(QAToolUtil.JDBC_PARAM_DRIVER));
-
-
-			System.setProperty(PersistenceManager.JDBC_PARAM_URL, "jdbc:oracle:thin:@slc10uam.us.oracle.com:1521:orcl12c");
-			System.setProperty(PersistenceManager.JDBC_PARAM_USER, "SYSEMS_T_1010");
-			System.setProperty(PersistenceManager.JDBC_PARAM_PASSWORD, "welcome1");
-			System.setProperty(PersistenceManager.JDBC_PARAM_DRIVER, "oracle.jdbc.OracleDriver");
-//		}
+		if (props != null) {
+			System.setProperty(PersistenceManager.JDBC_PARAM_URL, props.getProperty(QAToolUtil.JDBC_PARAM_URL));
+			System.setProperty(PersistenceManager.JDBC_PARAM_USER, props.getProperty(QAToolUtil.JDBC_PARAM_USER));
+			System.setProperty(PersistenceManager.JDBC_PARAM_PASSWORD, props.getProperty(QAToolUtil.JDBC_PARAM_PASSWORD));
+			System.setProperty(PersistenceManager.JDBC_PARAM_DRIVER, props.getProperty(QAToolUtil.JDBC_PARAM_DRIVER));
+		}
 	}
 }
