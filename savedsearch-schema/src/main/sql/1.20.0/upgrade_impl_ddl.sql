aa24d1057cea21c3ad514e401b77a96882f5b60b
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-06-26 15:03:43 +0800

    EMCPSSF-504

--- savedsearch-schema/src/main/sql/1.20.0/upgrade_impl_ddl.sql
+++ savedsearch-schema/src/main/sql/1.20.0/upgrade_impl_ddl.sql
@@ -5,7 +5,7 @@ Rem    XIADAI    06/01/2017 - created
 Rem
 Rem
 
-@&EMSAAS_SQL_ROOT/1.20.0/emaas_create_table_constraint.sql
+@&EMSAAS_SQL_ROOT/1.20.0/emaas_remove_table_constraint.sql
 @&EMSAAS_SQL_ROOT/1.20.0/create_table_resource_bundle.sql
 
 
