224b83dc914b56e759f11e0780bc02e6f0b52397
rex.liang <rex.liang@oracle.com>
2017-06-28 01:11:26 -0700

    EMCPSSF-504: move the SQL into 1.21.0

--- savedsearch-schema/src/main/sql/1.20.0/upgrade_impl_dml.sql
+++
@@ -1,9 +0,0 @@
-Rem
-Rem    MODIFIED   (MM/DD/YY)
-Rem    XIADAI     06/01/2017 - created
-Rem
-@&EMSAAS_SQL_ROOT/1.20.0/clear_up.sql
-@&EMSAAS_SQL_ROOT/1.20.0/emaas_pub_tenant_onboarding.sql -11 &EMSAAS_SQL_ROOT
-COMMIT;
-
-
