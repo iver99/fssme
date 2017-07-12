127485a38f0f4d0a09b1da811017ca4f5e4a7083
rex.liang <rex.liang@oracle.com>
2017-07-11 21:05:29 -0700

    EMCPSSF-519: create index for search params to improve performance

--- savedsearch-schema/src/main/sql/1.21.0/upgrade_impl_ddl.sql
+++ savedsearch-schema/src/main/sql/1.21.0/upgrade_impl_ddl.sql
@@ -8,4 +8,4 @@ Rem
 @&EMSAAS_SQL_ROOT/1.21.0/emaas_remove_table_constraint.sql
 @&EMSAAS_SQL_ROOT/1.21.0/create_table_resource_bundle.sql
 
-
+@&EMSAAS_SQL_ROOT/1.21.0/create_ems_search_params_idx.sql
