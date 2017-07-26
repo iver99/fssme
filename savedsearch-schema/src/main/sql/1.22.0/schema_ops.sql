018d36cddba35873927359c3df3187dae81a5892
ping.w.wu@oracle.com <pingwu@slc09wob.us.oracle.com>
2017-07-26 00:02:10 -0700

    update zdt sql version

---
+++ savedsearch-schema/src/main/sql/1.22.0/schema_ops.sql
@@ -0,0 +1,13 @@
+Rem ----------------------------------------------------------------
+Rem
+Rem 07/26/2017	PINGWU	    created
+Rem ----------------------------------------------------------------
+
+@&EMSAAS_SQL_ROOT/1.22.0/upgrade_impl_ddl.sql
+
+SET HEADING OFF
+SET FEEDBACK OFF
+SET LINESIZE 2000
+
+@&EMSAAS_SQL_ROOT/1.22.0/upgrade_impl_dml.sql
+
---
+++ savedsearch-schema/src/main/sql/1.22.0/upgrade_impl_dml.sql
@@ -0,0 +1,7 @@
+Rem
+Rem    MODIFIED   (MM/DD/YY)
+Rem    PINGWU     07/26/2017 - created
+Rem
+
+
+
