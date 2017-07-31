81e1add649a8cd16d3fe4c10d2597384d12fc996
rex.liang <rex.liang@oracle.com>
2017-07-31 03:39:32 -0700

    EMCPSSF-504: loop 10 times in case the service has not started yet when it calls SSF to refresh OOB metadata

--- savedsearch-web/src/main/java/oracle/sysman/emSDK/emaas/platform/savedsearch/ws/rest/thread/OobRefreshRunnable.java
+++ savedsearch-web/src/main/java/oracle/sysman/emSDK/emaas/platform/savedsearch/ws/rest/thread/OobRefreshRunnable.java
@@ -1,25 +1,26 @@
 package oracle.sysman.emSDK.emaas.platform.savedsearch.ws.rest.thread;
 
+import java.util.List;
+
 import oracle.sysman.SDKImpl.emaas.platform.savedsearch.model.SearchImpl;
 import oracle.sysman.SDKImpl.emaas.platform.savedsearch.model.SearchManagerImpl;
 import oracle.sysman.emSDK.emaas.platform.savedsearch.exception.EMAnalyticsDatabaseUnavailException;
 import oracle.sysman.emSDK.emaas.platform.savedsearch.exception.EMAnalyticsFwkException;
-import oracle.sysman.emSDK.emaas.platform.savedsearch.model.SearchManager;
 import oracle.sysman.emSDK.emaas.platform.savedsearch.model.TenantContext;
 import oracle.sysman.emSDK.emaas.platform.savedsearch.model.TenantInfo;
 import oracle.sysman.emaas.platform.savedsearch.metadata.MetaDataRetriever;
 import oracle.sysman.emaas.platform.savedsearch.metadata.MetaDataStorer;
 import oracle.sysman.emaas.platform.savedsearch.services.DependencyStatus;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.List;
 
 /**
  * Created by xiadai on 2017/6/14.
  */
 public class OobRefreshRunnable extends MetadataRefreshRunnable{
     private static final Logger LOGGER = LogManager.getLogger(OobRefreshRunnable.class);
+    
     @Override
     public void run(){
         if(serviceName == null || serviceName.isEmpty()) {
@@ -31,6 +32,15 @@ public class OobRefreshRunnable extends MetadataRefreshRunnable{
             if (!DependencyStatus.getInstance().isDatabaseUp()) {
                 throw new EMAnalyticsDatabaseUnavailException();
             }
+            int loopNum = 0;  // how many times SSF has already tried to fetch the OOB
+            while(!isServiceAvailable(serviceName, "1.0+") && (loopNum < MAX_LOOP_NUM)) {
+                try {
+                    LOGGER.warn("{} time failed to fecth OOB widgets from {}", loopNum++, serviceName);
+                    Thread.sleep(INTERVAL_TIME); // 60s
+                } catch (InterruptedException e) {
+                    LOGGER.error(e.getLocalizedMessage(), e);
+                }
+            }
             MetaDataRetriever metaDataRetriever = new MetaDataRetriever();
             oobWidgetList = metaDataRetriever.getOobWidgetListByServiceName(serviceName);
             TenantContext.setContext(new TenantInfo(SearchManagerImpl.DEFAULT_CURRENT_USER, SearchManagerImpl.DEFAULT_TENANT_ID));
@@ -39,4 +49,5 @@ public class OobRefreshRunnable extends MetadataRefreshRunnable{
             LOGGER.error(e.getLocalizedMessage());
         }
     }
+    
 }
