b19841f08efc0ec75e4563015a101adaed472a4c
ping.w.wu@oracle.com <pingwu@slc09wob.us.oracle.com>
2017-08-09 21:07:56 -0700

    add more test code

---
+++ savedsearch-comparator/savedsearchcomparator-web/src/integTest/java/oracle/sysman/emaas/platform/savedsearch/comparator/ws/timer/SavedsearchComparatorTriggerServiceTest.java
@@ -0,0 +1,11 @@
+package oracle.sysman.emaas.platform.savedsearch.comparator.ws.timer;
+
+import org.testng.annotations.Test;
+
+@Test(groups = { "s1" })
+public class SavedsearchComparatorTriggerServiceTest {
+	@Test
+	public void testService() {
+		SavedsearchComparatorTriggerService service = new SavedsearchComparatorTriggerService();
+	}
+}
