d88518a742c98c85a95273726fc5400afd5ad5e6
rex.liang <rex.liang@oracle.com>
2017-09-21 07:09:52 -0700

    EMCPSSF-591: update oob time to avoid ZDT sych

--- savedsearch-schema/src/main/sql/1.23.0/upgrade_impl_dml.sql
+++ savedsearch-schema/src/main/sql/1.23.0/upgrade_impl_dml.sql
@@ -4,5 +4,6 @@ Rem    REX     08/18/2017 - created
 Rem
 
 @&EMSAAS_SQL_ROOT/1.23.0/emaas_savedsearch_seed_data_monitoringui.sql
+@&EMSAAS_SQL_ROOT/1.23.0/emaas_savedsearch_update_oob_time.sql
 COMMIT;
 
