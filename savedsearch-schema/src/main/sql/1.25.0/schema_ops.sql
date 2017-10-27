69bf4c565528ec4d5ff8814c6cd9503604c6edee
Jiangxia Nan <jiangxia.nan@oracle.com>
2017-10-26 19:04:36 -0700

    move schema change to 1.25 folder

--- savedsearch-schema/src/main/sql/1.25.0/schema_ops.sql
+++ savedsearch-schema/src/main/sql/1.25.0/schema_ops.sql
@@ -3,11 +3,11 @@ Rem
 Rem 08/18/2017	REX	    created
 Rem ----------------------------------------------------------------
 
-@&EMSAAS_SQL_ROOT/1.24.0/upgrade_impl_ddl.sql
+@&EMSAAS_SQL_ROOT/1.25.0/upgrade_impl_ddl.sql
 
 SET HEADING OFF
 SET FEEDBACK OFF
 SET LINESIZE 2000
 
-@&EMSAAS_SQL_ROOT/1.24.0/upgrade_impl_dml.sql
+@&EMSAAS_SQL_ROOT/1.25.0/upgrade_impl_dml.sql
 
