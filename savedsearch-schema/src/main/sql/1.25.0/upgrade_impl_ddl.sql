8f9f1e56f673c4d1b616fd1e276094c6e8559b54
rex.liang <rex.liang@oracle.com>
2017-11-14 05:18:03 -0800

    EMCPSSF-643: Indexes with a tenant_id key column must list it first

--- savedsearch-schema/src/main/sql/1.25.0/upgrade_impl_ddl.sql
+++ savedsearch-schema/src/main/sql/1.25.0/upgrade_impl_ddl.sql
@@ -21,10 +21,34 @@ BEGIN
   EXCEPTION
   WHEN OTHERS THEN
     ROLLBACK;
+    DBMS_OUTPUT.PUT_LINE('>>>SSF DDL ERROR<<<');
     DBMS_OUTPUT.PUT_LINE('Failed to run the sql for federated dashboard support due to: '||SQLERRM);
     RAISE;
 END;
 /
 
-
+DECLARE
+  v_count_index        INTEGER;
+  v_count_position     INTEGER;
+BEGIN
+  --OMCSQ048: Indexes with a tenant_id key column must list it first
+  SELECT COUNT(1) INTO v_count_index FROM user_constraints WHERE constraint_name = 'EMS_ANALYTICS_LAST_ACCESS_PK' AND table_name = 'EMS_ANALYTICS_LAST_ACCESS';
+  SELECT COUNT(1) INTO v_count_position FROM all_ind_columns WHERE INDEX_NAME = 'EMS_ANALYTICS_LAST_ACCESS_PK' AND COLUMN_NAME = 'TENANT_ID' AND COLUMN_POSITION = 1;
+  IF v_count_index=0 THEN
+    DBMS_OUTPUT.PUT_LINE('constraint EMS_ANALYTICS_LAST_ACCESS_PK does not exist');
+  ELSIF  v_count_position=1 THEN
+    DBMS_OUTPUT.PUT_LINE('TENANT_ID has been already listed first in EMS_ANALYTICS_LAST_ACCESS_PK');
+  ELSE
+    EXECUTE IMMEDIATE 'ALTER TABLE EMS_ANALYTICS_LAST_ACCESS DROP CONSTRAINT EMS_ANALYTICS_LAST_ACCESS_PK';
+    EXECUTE IMMEDIATE 'ALTER TABLE EMS_ANALYTICS_LAST_ACCESS ADD CONSTRAINT EMS_ANALYTICS_LAST_ACCESS_PK PRIMARY KEY(TENANT_ID,OBJECT_ID,ACCESSED_BY,OBJECT_TYPE)';
+    DBMS_OUTPUT.PUT_LINE('TENANT_ID is listed first in EMS_ANALYTICS_LAST_ACCESS_PK successfully!');
+  END IF;
+  EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    DBMS_OUTPUT.PUT_LINE('>>>SSF DDL ERROR<<<');
+    DBMS_OUTPUT.PUT_LINE('Failed to rebuild index due to error '||SQLERRM);
+    RAISE;
+END;
+/
 
