74af75dbbc1c42800068200827ee02c802cd1bb7
rex.liang <rex.liang@oracle.com>
2017-11-20 00:59:34 -0800

    EMCPSSF-636: move the SQL into 1.26 folder

--- savedsearch-schema/src/main/sql/1.26.0/schema_ops.sql
+++ savedsearch-schema/src/main/sql/1.26.0/schema_ops.sql
@@ -1,11 +1,13 @@
 Rem ----------------------------------------------------------------
 Rem
-Rem 08/18/2017	REX	    created
+Rem 11/20/2017	REX	    created
 Rem ----------------------------------------------------------------
 
-@&EMSAAS_SQL_ROOT/1.25.0/upgrade_impl_ddl.sql
+@&EMSAAS_SQL_ROOT/1.26.0/upgrade_impl_ddl.sql
 
 SET HEADING OFF
 SET FEEDBACK OFF
 SET LINESIZE 2000
+
+@&EMSAAS_SQL_ROOT/1.26.0/upgrade_impl_dml.sql
 
---
+++ savedsearch-schema/src/main/sql/1.26.0/upgrade_impl_ddl.sql
@@ -0,0 +1,7 @@
+Rem
+Rem    DDL change during upgrade
+Rem    MODIFIED   (MM/DD/YY)
+Rem    REX   11/20/2017 - created
+Rem
+Rem
+
