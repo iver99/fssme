f20b200e923a2cb8e7b4abfeb694ad49bc2fb5d9
rex.liang <rex.liang@oracle.com>
2017-11-21 00:33:39 -0800

    EMCPSSF-643: move to 1.26 and drop index and compress index

---
+++ savedsearch-schema/src/main/sql/1.26.0/schema_ops.sql
@@ -0,0 +1,11 @@
+Rem ----------------------------------------------------------------
+Rem
+Rem 11/21/2017	REX	    created
+Rem ----------------------------------------------------------------
+
+@&EMSAAS_SQL_ROOT/1.26.0/upgrade_impl_ddl.sql
+
+SET HEADING OFF
+SET FEEDBACK OFF
+SET LINESIZE 2000
+
