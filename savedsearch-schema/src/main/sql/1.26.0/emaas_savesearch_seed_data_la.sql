b7d8340243d07ea0f7bea2d538fa8ae6f23976ef
Miao Yu <miao.yu@oracle.com>
2017-12-01 23:02:51 +0800

    add required change to fix bug: 27085336

---
+++ savedsearch-schema/src/main/sql/1.26.0/emaas_savesearch_seed_data_la.sql
@@ -0,0 +1,20 @@
+Rem
+Rem emaas_savedsearch_seed_data_la.sql
+Rem
+Rem Copyright (c) 2013, 2014, 2015, 2016, 2017 Oracle and/or its affiliates.
+Rem All rights reserved.
+Rem
+Rem    NAME
+Rem      emaas_savedsearch_seed_data_la.sql
+Rem
+Rem    DESCRIPTION
+Rem
+Rem    NOTES
+Rem      None
+Rem
+Rem    MODIFIED   (MM/DD/YY)
+Rem    
+Rem
+SET FEEDBACK ON
+SET SERVEROUTPUT ON
+
